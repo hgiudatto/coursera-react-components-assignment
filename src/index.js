ffb1457a52bb0c0f3dac0a6f8be414362d5d6ca1
Hector <hector.giudatto@gmail.com>
2022-10-18 09:22:54 -0300

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-social/bootstrap-social.css";
 import "./index.css";
 import App from "./App";
 const root = createRoot(document.getElementById("root"));
